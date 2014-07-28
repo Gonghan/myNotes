155aea92a9c41ccaea088e692451b047d749996f
Gonghan <wanggonghan@gmail.com>
2014-07-27 20:37:53 -0700

    update the index description

--- index.php
+++ index.php
@@ -7,8 +7,7 @@ include './template/header.php';
     <div class="jumbotron up70">
         <h1>MyNotes</h1>
 
-        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It
-            includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+        <p>The next-generation note-taking web app.</p>
 
         <p>
             <a class="btn btn-lg btn-primary" href="#" role="button">View navbar docs &raquo;</a>
