989bead847b47c436abc29721324dd343f282f66
Gonghan <wanggonghan@gmail.com>
2014-07-27 20:26:48 -0700

    update the wrong parameters

--- core/api/shownotes.php
+++ core/api/shownotes.php
@@ -1,8 +1,8 @@
 <?php
 include_once '../note.php';
 session_start();
-$note=new Note;
-$arr=$note->getNotes();
+$note = new Note();
+$arr = $note->getNotes();
 //var_dump($arr);
 echo json_encode($arr);
 ?>
