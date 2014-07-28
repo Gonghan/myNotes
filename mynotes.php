c34ce0d786f76bf48a59d236bea89077452e297c
Gonghan <wanggonghan@gmail.com>
2014-07-28 11:58:47 -0700

    fix panel index bug

--- mynotes.php
+++ mynotes.php
@@ -13,123 +13,131 @@ include_once './template/header.php';
             <button class="btn btn-primary">Add</button>
             <button class="btn btn-default">Cancel</button>
         </div>
-        <div id="display-notes" class="col-sm-offset-2 col-sm-8">
+        <div id="display-notes" class="col-sm-offset-2 col-sm-9">
             <h2><p class="text-info">All notes of <b><?php echo $_SESSION['name']; ?></b></p></h2>
-            <ul id="grid" class="list-inline">
-            </ul>
+            <div class="col-sm-4" id="div1">
+
+            </div>
+            <div class="col-sm-4" id="div2">
+
+            </div>
+            <div class="col-sm-4" id="div3">
+
+            </div>
         </div>
     </div>
-</div>
-<script>
-    makeActive(1);
-    $(function () {
-        <?php
-            if(!empty($_SESSION['name'])){
-        ?>
-        showNotes();
-        <?php
-            }else{
-        ?>
-        showLoginNotification();
-        <?php
-            }
-        ?>
-    });
-    var list = $('#grid');
-    var title = $('#new-note input');
-    var output = $('#output');
-    output.hide();
-    title.hide();
-    var addnote_btn = $('#new-note button').first().hide();
-    var cancelnote_btn = $('#new-note button').last().hide();
-    var content = $('#new-note textarea');
-    showAllNotes();
-    content.on('click', function (event) {
-        event.stopPropagation();
-        showNewNote();
-    });
-    title.on('click',function(event){
-        event.stopPropagation();
-    });
-    cancelnote_btn.on('click', function () {
-        clearNewNote();
-    });
-    addnote_btn.on('click', function () {
-        addNote(title.val(), content.val());
-    });
-    $(document.body).on('click',function(){
-        clearNewNote();
-    });
-    function showNote(note) {
-        var newrow = '<div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">' + note.title + '</h3></div><div class="panel-body">' + note.content + '</div><div class="panel-footer">' + note.created_at + '</div></div>';
-        //console.log(newrow);
-        list.append($('<li>'+newrow+'</li>'));
-        list.find('li').addClass('col-sm-4');
-    }
-    function showAllNotes() {
-        removeList();
-        $.post('./core/api/shownotes.php', function (data) {
-            var json_obj = $.parseJSON(data);
-            for (var j in json_obj) {
-                var i = json_obj.length - j - 1;
-                showNote(json_obj[i]);
-            }
+    <script>
+        makeActive(1);
+        $(function () {
+            <?php
+                if(!empty($_SESSION['name'])){
+            ?>
+            showNotes();
+            <?php
+                }else{
+            ?>
+            showLoginNotification();
+            <?php
+                }
+            ?>
         });
-    }
-    function removeList() {
-        list.find('li').remove();
-    }
-    function addNote(t, c) {
-        var request = $.ajax({
-            type: 'POST',
-            data: {title: title.val(), content: content.val()},
-            url: './core/api/addnote.php'
-        });
-        request.done(function () {
-            output.text('Well Done! You successfully add a note.');
-            clearNewNote();
-            output.show();
-            showAllNotes();
-        });
-    }
-    function showNewNote() {
-        content.attr('rows', 3);
-        title.show();
-        addnote_btn.show();
-        cancelnote_btn.show();
+        var list = $('#grid');
+        var title = $('#new-note input');
+        var output = $('#output');
         output.hide();
-    }
-    function clearNewNote() {
-        content.attr('rows', 1);
-        content.val('');
-        title.val('');
         title.hide();
-        output.hide();
-        addnote_btn.hide();
-        cancelnote_btn.hide();
-    }
-    function showLoginNotification() {
-        $('#login-notification').show();
-        $('#notes').hide();
-    }
-    function showNotes() {
-        $('#login-notification').hide();
-        $('#notes').show();
-        $('#display-notes').show();
-    }
-</script>
-<style>
-    body {
-        padding-top: 70px;
-    }
+        var addnote_btn = $('#new-note button').first().hide();
+        var cancelnote_btn = $('#new-note button').last().hide();
+        var content = $('#new-note textarea');
+        showAllNotes();
+        content.on('click', function (event) {
+            event.stopPropagation();
+            showNewNote();
+        });
+        title.on('click', function (event) {
+            event.stopPropagation();
+        });
+        cancelnote_btn.on('click', function () {
+            clearNewNote();
+        });
+        addnote_btn.on('click', function () {
+            addNote(title.val(), content.val());
+        });
+        $(document.body).on('click', function () {
+            clearNewNote();
+        });
+        function showNote(note, index) {
+            //console.log(index+"|"+note.title);
+            var newrow = '<div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">' + note.title + '</h3></div><div class="panel-body">' + note.content + '</div><div class="panel-footer">' + note.created_at + '</div></div>';
+            var id = "#div" + (index + 1);
+            $(id).append(newrow);
+            //console.log(id+"|"+note.title);
+        }
+        function showAllNotes() {
+            removeList();
+            $.post('./core/api/shownotes.php', function (data) {
+                var json_obj = $.parseJSON(data);
+                for (var j in json_obj) {
+                    var i = json_obj.length - j - 1;
+                    showNote(json_obj[i], j%3);
+                }
+            });
+        }
+        function removeList() {
+            $('.panel').remove();
+        }
+        function addNote(t, c) {
+            var request = $.ajax({
+                type: 'POST',
+                data: {title: title.val(), content: content.val()},
+                url: './core/api/addnote.php'
+            });
+            request.done(function () {
+                output.text('Well Done! You successfully add a note.');
+                clearNewNote();
+                output.show();
+                showAllNotes();
+            });
+        }
+        function showNewNote() {
+            content.attr('rows', 3);
+            title.show();
+            addnote_btn.show();
+            cancelnote_btn.show();
+            output.hide();
+        }
+        function clearNewNote() {
+            content.attr('rows', 1);
+            content.val('');
+            title.val('');
+            title.hide();
+            output.hide();
+            addnote_btn.hide();
+            cancelnote_btn.hide();
+        }
+        function showLoginNotification() {
+            $('#login-notification').show();
+            $('#notes').hide();
+        }
+        function showNotes() {
+            $('#login-notification').hide();
+            $('#notes').show();
+            $('#display-notes').show();
+        }
+    </script>
+    <style>
+        body {
+            padding-top: 70px;
+        }
 
-    textarea {
-        resize: none;
-    }
-    #grid li{
+        textarea {
+            resize: none;
+        }
 
-    }
-</style>
-<?php
-include_once './template/footer.php';
-?>
+        #grid li {
+
+        }
+    </style>
+    <?php
+    include_once './template/footer.php';
+    ?>
