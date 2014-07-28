5e86c2007d3a443798c74d421ac6541148ff5d05
Gonghan <wanggonghan@gmail.com>
2014-07-27 20:28:42 -0700

    includes thing, remove unused dropdown

--- template/header.php
+++ template/header.php
@@ -10,6 +10,8 @@ session_start();
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <script src="./template/script.js"></script>
+    <link rel="shortcut icon" href="https://ssl.gstatic.com/keep/keep.ico">
+    <title>Notes</title>
 </head>
 <body>
 <div class="navbar navbar-default navbar-fixed-top" role="navigation">
@@ -28,18 +30,6 @@ session_start();
                 <li class="active"><a href="./">Home</a></li>
                 <li><a href="./mynotes.php">My Notes</a></li>
                 <li><a href="./core/api/setup.php">Setup</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li class="divider"></li>
-                        <li class="dropdown-header">Nav header</li>
-                        <li><a href="#">Separated link</a></li>
-                        <li><a href="#">One more separated link</a></li>
-                    </ul>
-                </li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <?php
